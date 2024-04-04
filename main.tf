a5603e2150f2bfd9dd7a0790c433c69b2ff9f1f8
rajendra <rajendrammarboyana@gmail.com>
2024-04-04 10:52:57 +0100

    state file

--- main.tf
+++ main.tf
@@ -8,19 +8,19 @@ module "frontend" {
   zone_id = var.zone_id
 }
 
-module "frontend" {
+module "backend" {
   source = "./modules/app"
   instance_type = var.instance_type
-  component = "frontend"
+  component = "backend"
   ssh_user = var.ssh_pass
   ssh_pass = var.ssh_pass
   env = var.env
   zone_id = var.zone_id
 }
-module "frontend" {
+module "mysql" {
   source = "./modules/app"
   instance_type = var.instance_type
-  component = "frontend"
+  component = "mysql"
   ssh_user = var.ssh_pass
   ssh_pass = var.ssh_pass
   env = var.env
