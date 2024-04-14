803c477c1cbe1ec9c36584016b9dd85e9e65f487
rajendra <rajendrammarboyana@gmail.com>
2024-04-14 14:34:51 +0100

    updating ~/ home sym

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -24,6 +24,7 @@ resource "null_resource" "ansible" {
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=${var.vault_token}",
       "ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @~/secrets.json -e @~/app.json"
+      "rm -f ~/secrets.json ~/app.json"
 
     ]
   }
