33622e5bc3e71bde1bb7af4cfdd89c2499391b37
rajendra <rajendrammarboyana@gmail.com>
2024-04-14 10:50:32 +0100

    install h ac module to talk to ansible

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -21,7 +21,7 @@ resource "null_resource" "ansible" {
       host     = aws_instance.instance.private_ip
     }
     inline = [
-      "sudo pip3.11 install ansible",
+      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component} -e vault_token=$(var.vault_token)",
       "ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @common.json -e @${var.component}.json"
 
