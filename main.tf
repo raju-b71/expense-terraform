006f76cd01228b01a9d7f2eeb2b45e443e6910a4
rajendra <rajendrammarboyana@gmail.com>
2024-04-20 06:45:27 +0100

    ADD ROUTE TABLES ROUTES ALLOWING CONNECTION WITH PERING

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "vpc" {
   subnet_cidr_block = var.subnet_cidr_block
   default_vpc_id = var.default_vpc_id
   default_vpc_cidr = var.default_vpc_cidr
-
+  default_route_table_id = var.default_route_table_id
 }
 
 
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "main" {
     Name = "${var.env}-subnet"
   }
 }
-resource "aws_vpc_peering_connection" "foo" {
+resource "aws_vpc_peering_connection" "main" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept = true
@@ -21,5 +21,27 @@ resource "aws_vpc_peering_connection" "foo" {
     Name = "${var.env}-vpc-to-default-vpc"
   }
 
-
 }
+resource "aws_route" "main" {
+  route_table_id   = aws_vpc.main.default_route_table_id
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  destination_cidr_block = var.default_vpc_cidr
+}
+
+resource "aws_route" "default-vpc" {
+  route_table_id   = var.default_route_table_id
+  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  destination_cidr_block = var.vpc_cidr_block
+}
+
+
+
+
+
+
+
+
+
+
+
+
