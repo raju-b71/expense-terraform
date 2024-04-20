3318bb26da5dab449f69670675f47fb798e412eb
rajendra <rajendrammarboyana@gmail.com>
2024-04-20 17:04:07 +0100

    creating public subnets

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ module "vpc" {
   frontend_subnets = var.frontend_subnets
   backend_subnets = var.backend_subnets
   db_subnets = var.db_subnets
+  public_subnets = var.public_subnets
   availability_zones = var.availability_zones
 }
 
