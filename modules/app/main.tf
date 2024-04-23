dda4d1756903d1037ac29927085c335afb9b1a65
rajendra <rajendrammarboyana@gmail.com>
2024-04-23 11:28:47 +0100

    SO THATS HOW WE CAHANGE SECURITY GROUPS AND ALLOW LIMITED ACCESS.

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -27,8 +27,8 @@ resource "aws_security_group" "main" {
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"                 #one is outboundport
-    cidr_blocks      = [var.bastion_nodes]
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags = {
