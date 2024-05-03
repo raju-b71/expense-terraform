a0fa52f4546e4dffd676decc89dff670972332ef
rajendra <rajendrammarboyana@gmail.com>
2024-05-03 08:09:55 +0100

    tune ansible and not to run

--- modules/app-asg/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "golden-ami-*"
-  owners      = ["self"]
-}
-
-data "vault_generic_secret" "ssh" {
-  path = "common/common"
-}
--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,5 +1,4 @@
+#!/bin/bash
 
-#/bin/bash
-
-ansible-pull -i localhost, -U https://github.com/raghudevopsb78/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/raghudevopsb78/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json -e only_deployment=false  &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json -e only_deployment=false  &>>/opt/ansible.log
