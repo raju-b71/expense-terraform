f6ab8560362b5c781c5b43a13b9211536c7e6083
rajendra <rajendrammarboyana@gmail.com>
2024-05-03 18:47:11 +0100

    enabling frontend

--- main.tf
+++ main.tf
@@ -46,6 +46,23 @@
 # #
 # }
 
+module "frontend" {
+  source = "./modules/app-asg"
+  app_port = 80
+  bastion_nodes = var.bastion_nodes
+  component = "backend"
+  env = var.env
+  instance_type = var.instance_type
+  max_capacity = var.max_capacity
+  min_capacity =var.min_capacity
+  prometheus_nodes = var.prometheus_nodes
+  server_app_port_sg_cidr = concat(var.frontend_subnets, var.backend_subnets)
+  subnets = module.vpc.frontend_subnets
+  vpc_id = module.vpc.vpc_id
+  vault_token = var.vault_token
+
+}
+
 module "backend" {
   source = "./modules/app-asg"
   app_port = 8080
