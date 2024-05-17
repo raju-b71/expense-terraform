414da40f8afc03b05086f667f186c467c9aa65b5
rajendra <rajendrammarboyana@gmail.com>
2024-05-17 10:18:15 +0100

    deleting ami creation After creating

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>>/opt/ansible.log
+
 ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible get-secrets.yml -e env=${env} -e role_name=${component} -e vault_token=${vault_token} &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/raju-b71/expense-ansible expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json -e only_deployment=false &>>/opt/ansible.log
 
